a78a20b425c56286316f0095c4dfcfb5b34e03f0
XuChenXu <91937041+ChenXu233@users.noreply.github.com>
2026-07-23 20:35:45 +0800

    fix: pretty-print cache JSON with line breaks for readable git diffs

--- src/utils.ts
+++ src/utils.ts
@@ -4,4 +4,4 @@ import fs from "fs-extra";
 export const getFileHash = (content: string) => crypto.MD5(content).toString();
 export const loadCache = async (p: string) =>
   (await fs.pathExists(p)) ? fs.readJson(p) : {};
-export const saveCache = async (p: string, data: any) => fs.writeJson(p, data);
+export const saveCache = async (p: string, data: any) => fs.writeJson(p, data, { spaces: 2 });
